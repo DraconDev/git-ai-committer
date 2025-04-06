79994cfe0da29855bf7e63d99eb48024d95663a0
DraconDev <dracsharp@gmail.com>
2025-04-06 22:21:50 +0100

    refactor: remove unused stageAllChanges import

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -11,7 +11,6 @@ import {
   enableVersionBumping,
   disableVersionBumping,
 } from "../version/versionService";
-import { stageAllChanges } from "../git/gitOperations";
 
 export function registerCommands(context: vscode.ExtensionContext) {
   context.subscriptions.push(
